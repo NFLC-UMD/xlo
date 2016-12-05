e208e919600ec6d9446160cd0fe7645faa643e53
Thomas Ford <tford@nflc.umd.edu>
2016-12-05 16:50:32 +0000

    index.js edited online with Bitbucket

--- index.js
+++ index.js
@@ -2,19 +2,24 @@
 
 class NflcLoPackager {
 
-    constructor (options) {
+    constructor (targetDir, louiSrcDir) {
+        this.targetDir = targetDir;
+        this.louiSrcDir = louiSrcDir;
+    }
+
+    //getters and setters 
+    
+    //options object read from targetDir/lopkg.json
+    set configOpts(options) {
         let opts = Object.assign({
-                targetDir: "",
-                LouiSrcDir: "",
                 language: null,
                 modality: null,
                 product: null,
                 lessonType: null,
                 runTimeEnv: ""
-            }, options);
+            }, options);   
+        this.apply(this, opts); //??
     }
-
-    //getters and setters 
 
     //methods
 
