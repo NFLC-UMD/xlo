3fc92f268fa10ee00bfb3319cdc547429febabda
thomaswford <tford@nflc.umd.edu>
2016-12-05 10:20:02 -0500

    stub class

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+class NflcLoPackager {
+
+    constructor (options) {
+        let opts = Object.assign({
+                targetDir: "",
+                LouiSrcDir: "",
+                language: null,
+                modality: null,
+                product: null,
+                lessonType: null,
+                runTimeEnv: ""
+            }, options);
+    }
+
+    //getters and setters 
+
+    //methods
+
+}
+
+module.exports = NflcLoPackager; 
